cf671e87e6954d3e93084aee01d055a50e2e2525
24Syleo <sylvain.beggiora@outlook.com>
2023-05-26 17:58:55 +0200

    Start ignoring config.js

--- libs/config.js
+++
@@ -1,7 +0,0 @@
-const urlDiplome = "http://localhost:8090/cv_informatique/diplomes/";
-const urlCompetence = "http://localhost:8090/cv_informatique/competences";
-
-export {
-    urlDiplome,
-    urlCompetence
-}
