b2565a2e6f65da7288196b089b5f473c09350470
24Syleo <sylvain.beggiora@outlook.com>
2023-05-26 18:00:21 +0200

    gitignore

---
+++ libs/config.js
@@ -0,0 +1,7 @@
+const urlDiplome = "http://localhost:8090/cv_informatique/diplomes/";
+const urlCompetence = "http://localhost:8090/cv_informatique/competences";
+
+export {
+    urlDiplome,
+    urlCompetence
+}
