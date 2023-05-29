1c0eb71ff67b52dd99aae61125b15f5c3640b8a6
24Syleo <sylvain.beggiora@outlook.com>
2023-05-29 20:52:41 +0200

    css competences

--- controller/Competences.js
+++ controller/Competences.js
@@ -7,7 +7,7 @@ class Competences {
 
     constructor() {
         this._axios = new Axios;
-        this._carEl = document.getElementById("carousselList");
+        this._carEl = document.getElementById("rowImage");
     }
 
     async getCompetences() {
@@ -32,29 +32,7 @@ class Competences {
         this._carEl;
         for (let i of value) {
             console.log(i);
-            const div = document.createElement("div");
-            div.classList.add("caroussel-item", "active");
-            const img = document.createElement("img");
-            img.src = "./pics/" + i.image;
-            img.classList.add("d-block", "w-100");
-            const divCont = document.createElement("div");
-            divCont.classList.add("container", "d-flex", "justify-content-center", "align-content-center");
-            const divRow = document.createElement("div");
-            divRow.classList.add("row");
-            const h2 = document.createElement("h2");
-            h2.innerText = i.titre;
-            const p = document.createElement("p");
-            p.innerText = i.description;
-            const a = document.createElement("a");
-            a.innerText = i.titre;
-            a.setAttribute("href", i.lien);
-            divRow.appendChild(h2);
-            divRow.appendChild(p);
-            divRow.appendChild(a);
-            divCont.appendChild(divRow);
-            div.appendChild(img);
-            div.appendChild(divCont);
-            this._carEl.appendChild(div);
+
         }
     }
 }
